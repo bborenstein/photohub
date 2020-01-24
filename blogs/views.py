355843587e189229c9fc99215254ccf80a3cdb99
Bryan Borenstein <pcabryan@protonmail.com>
2020-01-24 11:22:38 -0500

    Pagionation Functioning

--- blogs/views.py
+++ blogs/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+# Import Posting & Photographer Models To Bring In Data To Contact Us (Photographers) & Home/Index (Featured Posts)
+from postings.models import Posting
+from photographers.models import Photographer
 
 # Create Functions To Render html Template Files
-# Allows urls.py to call views by name
+# Allows 'urls.py' To Call Views By Name
 def index(request):
-    return render(request, 'blogs/index.html')
-
-def portfolio(request):
-    return render(request, 'blogs/portfolio.html')
+    # [:3] Limits Featured To Only Most Recent 3 Postings
+    postings = Posting.objects.order_by('-posted_date').filter(is_published=True)[:3]
+    # Fetch Schema Data (Postings) As Variable 'context'
+    context = {
+        'postings': postings
+    }
+    # Pass In Data As Variable For Feature Loop In HTML
+    return render(request, 'blogs/index.html', context)
 
 def contact(request):
-    return render(request, 'blogs/contact.html')
+    # Get All Photographers In Database
+    photographers = Photographer.objects.order_by('-hire_date')
+
+    context = {
+        'photographers': photographers
+    }
+
+    return render(request, 'blogs/contact.html', context)
